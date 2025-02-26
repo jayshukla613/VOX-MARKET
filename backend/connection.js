531ffbf55ccabe5557602344e097ba8698f9c9d2
aman-s2003 <sharmaaman99319@gmail.com>
2025-02-26 13:59:22 +0530

    first commit

---
+++ backend/connection.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const url = "mongodb+srv://sharmaaman99319:Amans123@cluster0.gjdpn.mongodb.net/mydb?retryWrites=true&w=majority&appName=Cluster0"
+mongoose.connect(url)
+.then((result) => {
+    console.log('Database connected successfully')
+}).catch((err) => {
+    console.log(err)
+});
+module.exports = mongoose;
---
+++ backend/models/userModel.js
@@ -0,0 +1,10 @@
+const { Schema , model } = require('../connection');
+
+const mySchema = new Schema({
+    name : String,
+    email : {type: String, unique :true},
+    password : {type: String , require : true},
+    city : {type : String, default : 'unknown'},
+    createdAt : {type : Date, default: Date.now}
+})
+module.exports = model('users', mySchema);
